ac6989f79930a835f9ad3c0f197bc2dd59240b11
Bukunmi001 <ogundejibukunmi5@gmail.com>
2024-06-24 20:16:44 +0100

    Php test
    It is working perfectly

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = [
+    'client_id' => '906519987162-v2jk0nmuvdbr75va13qnjfngbo3kcvmi.apps.googleusercontent.com',
+    'client_secret' => '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',
+    'redirect_uri' => 'http://localhost/login/login.php',
+    'scopes' => ['email', 'profile'],
+];
